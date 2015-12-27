e7a5ee9ea742179150d13bb94e2cccf3e43db4ec
raduscrieciu <radu.scrieciu@gmail.com>
2015-12-27 18:07:47 +0000

    Initial commit

---
+++ app/src/main/java/com/screechstudios/compoundview/ArticleView.java
@@ -0,0 +1,63 @@
+package com.screechstudios.compoundview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class ArticleView extends RelativeLayout {
+
+    private TextView titleTextView;
+    private TextView contentTextView;
+    private LinearLayout contentLayout;
+    private ProgressBar loadingProgress;
+
+    public ArticleView(Context context) {
+        super(context);
+
+        initView(context);
+    }
+
+    public ArticleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        initView(context);
+    }
+
+    public ArticleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        initView(context);
+    }
+
+    private void initView(Context context) {
+        inflate(context, R.layout.view_article, this);
+
+        titleTextView = (TextView) findViewById(R.id.titleTextView);
+        contentTextView = (TextView) findViewById(R.id.contentTextView);
+        contentLayout = (LinearLayout) findViewById(R.id.contentLayout);
+        loadingProgress = (ProgressBar) findViewById(R.id.loadingProgressBar);
+    }
+
+    public void setLoading(boolean isLoading) {
+        if (isLoading) {
+            contentLayout.setVisibility(INVISIBLE);
+            loadingProgress.setVisibility(VISIBLE);
+        } else {
+            contentLayout.setVisibility(VISIBLE);
+            loadingProgress.setVisibility(INVISIBLE);
+        }
+    }
+
+    public void populate(String title, String content) {
+        titleTextView.setText(title);
+        contentTextView.setText(content);
+    }
+
+    public void setBackground(int color) {
+        contentLayout.setBackgroundColor(color);
+    }
+
+}
---
+++ app/src/main/java/com/screechstudios/compoundview/MainActivity.java
@@ -0,0 +1,39 @@
+package com.screechstudios.compoundview;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ArticleView articleView = (ArticleView) findViewById(R.id.articleView);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                populateArticle(articleView);
+            }
+        });
+
+    }
+
+    private void populateArticle(final ArticleView articleView) {
+        articleView.setLoading(true);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                articleView.setLoading(false);
+                articleView.setBackground(Color.LTGRAY);
+                articleView.populate("My First Article", "This is my first ever article, hope you enjoy!");
+            }
+        }, 3000);
+    }
+}
